1469e04f56fa19b4c3d45c7fb258952882d496db
laluferdianyusuf <lferdianyusuf@gmail.com>
2025-07-18 09:54:13 +0800

    update dependencies and update ui

--- components/CategoryItem.tsx
+++ components/CategoryItem.tsx
@@ -14,22 +14,18 @@ const CategoryItem: React.FC<CategoryItemProps> = ({ item, handlePress }) => {
 
   return (
     <Pressable
-      className={`mx-2 items-center rounded-2xl bg-custom-grey-1 `}
+      className={`rounded-3xl items-center justify-center border border-gray-100 flex-1 bg-custom-grey-1 `}
       onPress={handlePress}
     >
-      <View className={`p-4 flex-row items-center gap-3 w-full`}>
+      <View className={`p-4 py-6 items-center gap-3 flex-col `}>
         <View
-          className={`${item.color} p-2 rounded-2xl items-center justify-center`}
-          style={{
-            width: 50,
-            height: 50,
-          }}
+          className={`${item.color} w-20 h-20 p-2 rounded-2xl items-center justify-center`}
         >
           <Ionicons name={item.icon as any} size={30} color={"white"} />
         </View>
         <View>
-          <Text className="font-josefin-bold">{item.name}</Text>
-          <Text className="font-josefin text-sm">{item.desc}</Text>
+          <Text className="font-josefin-bold text-center">{item.name}</Text>
+          <Text className="font-josefin text-sm text-center">{item.desc}</Text>
         </View>
       </View>
     </Pressable>
